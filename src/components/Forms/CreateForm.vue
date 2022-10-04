e025910e4508d31b43e857e7a15b28d13eafb920
doluwamu <dboyadeitan@gmail.com>
2022-10-04 16:19:15 +0100

    made styling improvements to book create form

--- src/components/Forms/CreateForm.vue
+++ src/components/Forms/CreateForm.vue
@@ -1,35 +1,65 @@
 <template>
-  <form class="form-container">
+  <form class="form-container" @submit="handleAddBook">
     <div class="form-items">
       <div class="form-item book__name">
         <div class="label-wrapper">
           <label>Name:</label>
         </div>
-        <input type="text" placeholder="Write here..." />
+        <input
+          type="text"
+          name="name"
+          required
+          v-model="name"
+          placeholder="Write here..."
+        />
       </div>
       <div class="form-item book__image">
         <div class="label-wrapper">
           <label>Image:</label>
         </div>
-        <input type="text" placeholder="Write here..." />
+        <input
+          type="text"
+          name="image"
+          required
+          v-model="image"
+          placeholder="Write here..."
+        />
       </div>
       <div class="form-item book__author">
         <div class="label-wrapper">
           <label>Author:</label>
         </div>
-        <input type="text" placeholder="Write here..." />
+        <input
+          type="text"
+          name="author"
+          required
+          v-model="author"
+          placeholder="Write here..."
+        />
       </div>
       <div class="form-item book__publish__date">
         <div class="label-wrapper">
           <label>Publish date:</label>
         </div>
-        <input type="date" placeholder="Write here..." />
+        <input
+          type="date"
+          name="publish-date"
+          required
+          v-model="publishDate"
+          placeholder="Write here..."
+        />
       </div>
       <div class="form-item book__num__of__pages">
         <div class="label-wrapper">
           <label>Number of pages:</label>
         </div>
-        <input type="number" placeholder="Write here..." />
+        <input
+          type="number"
+          name="num-of-pages"
+          required
+          v-model="numOfPages"
+          placeholder="Write here..."
+        />
       </div>
       <div class="form-item book__preface">
         <div class="label-wrapper">
@@ -37,6 +67,8 @@
         </div>
         <textarea
           name="preface"
+          required
+          v-model="preface"
           placeholder="Write here..."
           cols="30"
           rows="10"
@@ -46,7 +78,17 @@
         <div class="label-wrapper">
           <label>Link:</label>
         </div>
-        <input type="text" placeholder="Write here..." />
+        <input
+          type="text"
+          name="link"
+          required
+          v-model="link"
+          placeholder="Write here..."
+        />
+      </div>
+
+      <div class="form-button">
+        <button type="submit">Add book</button>
       </div>
     </div>
   </form>
@@ -57,6 +99,32 @@ import "@/assets/styles/form.css";
 
 export default {
   name: "CreateForm",
+  data() {
+    return {
+      image: "",
+      name: "",
+      numOfPages: "",
+      author: "",
+      publishDate: "",
+      preface: "",
+      link: "",
+    };
+  },
+  methods: {
+    handleAddBook(e) {
+      e.preventDefault();
+      const bookData = {
+        image: this.image,
+        name: this.name,
+        numOfPages: Number(this.numOfPages),
+        author: this.author,
+        publishDate: this.publishDate,
+        preface: this.preface,
+        link: this.preface,
+      };
+      console.log(bookData);
+    },
+  },
 };
 </script>
 
