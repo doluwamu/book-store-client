03e1d47d4168a22e9151c7cdebcc73b6a99eab3a
doluwamu <dboyadeitan@gmail.com>
2022-10-08 00:45:16 +0100

    made styling improvements to home view component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
     <div class="home-titles">
-      <h3 class="home-title">Welcome To Book Store</h3>
-      <p class="home-subtitle">Thousands of books at your finger tips</p>
+      <div class="title-contents">
+        <h3 class="home-title">Welcome To Book Store</h3>
+        <p class="home-subtitle">Thousands of books at your finger tips</p>
+      </div>
     </div>
 
-    <div class="books-container">
-      <div v-for="(book, index) in allBooks" :key="index">
-        <div v-if="book && book !== null">
-          <BookCard :book="book" />
+    <div class="most-read">
+      <div class="most-read-title">
+        <h3>Recommended for you</h3>
+        <div class="mr-line line"></div>
+      </div>
+
+      <div class="books-container">
+        <div v-for="(book, index) in allBooks" :key="index">
+          <div v-if="book && book !== null">
+            <BookCard :book="book" />
+          </div>
         </div>
       </div>
     </div>
@@ -41,3 +50,36 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.home-titles {
+  background: url("@/assets/bg-img.avif");
+  background-color: rgb(50, 50, 50);
+  background-blend-mode: multiply;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 90vh;
+}
+
+.most-read {
+  margin-top: 30px;
+}
+
+.most-read-title {
+  max-width: 200px;
+  margin-left: 50px;
+  padding: 5px;
+}
+
+.most-read h3 {
+  font-weight: 500;
+  text-align: left;
+}
+
+.most-read .line {
+  background-color: #fff;
+  max-width: 100px;
+  height: 2px;
+  border-radius: 10px;
+}
+</style>
